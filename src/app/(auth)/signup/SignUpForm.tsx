fb8931aef58bf4da9b2659cadc3701cdecec67db
ksn199ms <mubashirpannur123@gmail.com>
2024-10-16 12:03:32 +0530

    SignUp form UI completed

---
+++ src/app/(auth)/signup/SignUpForm.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { signUpSchema, SignUpValues } from "@/lib/validation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState, useTransition } from "react";
+import { signUp } from "./actions";
+import { PasswordInput } from "@/components/ui/PasswordInput";
+import LoadingButton from "@/components/ui/LoadingButton";
+
+export default function SignUpForm() {
+
+    const [error, setError] = useState<string>();
+
+    const [isPending, startTransition] = useTransition();
+
+  const form = useForm<SignUpValues>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      email: "",
+      username: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: SignUpValues) {
+    setError(undefined);
+    startTransition(async () => {
+      const { error } = await signUp(values);
+      if (error) setError(typeof error === 'string' ? error : 'An unknown error occurred');
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+        {error && <p className="text-center text-destructive">{error}</p>}
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your username" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>E-mail</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your Email" type="email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <PasswordInput  placeholder="password" {...field}  />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <LoadingButton loading={isPending} type="submit" className="w-full">
+          Submit
+        </LoadingButton>
+      </form>
+    </Form>
+  );
+}
