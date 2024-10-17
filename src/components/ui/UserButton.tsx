76d090ea3eed02cd76139d815d6be9aa70711422
ksn199ms <mubashirpannur123@gmail.com>
2024-10-17 19:25:55 +0530

    clearing cache logic implemented

--- src/components/ui/UserButton.tsx
+++ src/components/ui/UserButton.tsx
@@ -19,6 +19,7 @@ import {
 import UserAvatar from "./UserAvatar";
 import { useTheme } from "next-themes";
 import { DropdownMenuSub } from "@radix-ui/react-dropdown-menu";
+import { useQueryClient } from "@tanstack/react-query";
 
 interface UserButtonProps {
   className?: string;
@@ -28,6 +29,8 @@ export default function UserButton({ className }: UserButtonProps) {
   const { user } = useSession();
 
   const {theme, setTheme} = useTheme();
+
+  const queryClient = useQueryClient();
 
   return (
     <DropdownMenu>
@@ -74,6 +77,7 @@ export default function UserButton({ className }: UserButtonProps) {
         <DropdownMenuSeparator />
         <DropdownMenuItem
           onClick={() => {
+            queryClient.clear();
             logout();
           }}
         >
