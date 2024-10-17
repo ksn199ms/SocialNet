2ff0ef2fe05013e4d7093fb55ec1493efa51fc7b
ksn199ms <mubashirpannur123@gmail.com>
2024-10-17 10:38:37 +0530

    MenuBar UI Implemented

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,20 +1,28 @@
-import { validateRequest } from "@/auth"
+import { validateRequest } from "@/auth";
 import { redirect } from "next/navigation";
 import SessionProvider from "./SessionProvider";
 import Navbar from "./Navbar";
+import MenuBar from "./MenuBar";
 
 export default async function Layout({
-    children,}: {
-        children: React.ReactNode
-    }) {
-        const session = await validateRequest();
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await validateRequest();
 
-        if(!session.user) redirect("/login");
+  if (!session.user) redirect("/login");
 
-        return <SessionProvider value={session}>
-            <div className="flex min-h-screen flex-col">
-            <Navbar />
-            <div className="mx-auto max-w-7xl p-5">{children}</div>
-            </div>
-        </SessionProvider>
-    }
+  return (
+    <SessionProvider value={session}>
+      <div className="flex min-h-screen flex-col">
+        <Navbar />
+        <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
+          <MenuBar className="sticky hidden h-fit flex-none space-y-3 rounded-2xl bg-card to-[5.25rem] px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
+          {children}
+        </div>
+        <MenuBar className="sticky bottom-0 flex w-full justify-center gap-6 border-t bg-card p-3 sm:hidden" />
+      </div>
+    </SessionProvider>
+  );
+}
