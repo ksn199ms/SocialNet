229beb0360bf78da11dd300d571f598f94982508
ksn199ms <mubashirpannur123@gmail.com>
2024-10-18 19:25:11 +0530

    Delete button integreated to the post component

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -2,14 +2,18 @@ import { PostData } from "@/lib/types";
 import { formatRelativeDate } from "@/lib/utils";
 import Link from "next/link";
 import UserAvatar from "../ui/UserAvatar";
+import { useSession } from "@/app/(main)/SessionProvider";
+import PostMoreButton from "./PostMoreButton";
 
 interface PostProps {
   post: PostData;
 }
 
 export default function Post({ post }: PostProps) {
+  const {user} = useSession();
   return (
-    <article className="space-y-3 rounded-2xl bg-card p-5 shadow-sm">
+    <article className="group/post space-y-3 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="flex justify-between gap-3">
       <div className="flex flex-wrap gap-3">
         <Link href={`/users/${post.user.username}`}>
           <UserAvatar avatarUrl={post.user.avatarUrl} />
@@ -28,6 +32,13 @@ export default function Post({ post }: PostProps) {
             {formatRelativeDate(post.createdAt)}
           </Link>
         </div>
+      </div>
+      {post.user.id === user.id && (
+          <PostMoreButton
+            post={post}
+            className="opacity-0 transition-opacity group-hover/post:opacity-100"
+          />
+        )}
       </div>
       <div className="whitespace-pre-line break-words">{post.content}</div>
     </article>
