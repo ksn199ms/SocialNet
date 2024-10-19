44c4fd5151bfd76e97c0b1c145f407019b85cf27
ksn199ms <mubashirpannur123@gmail.com>
2024-10-19 13:41:42 +0530

    Tabs section implemented to main page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,8 +2,9 @@ import PostEditor from "@/components/posts/editor/PostEditor";
 import Post from "@/components/posts/Post";
 import TrendsSidebar from "@/components/ui/TrendsSidebar";
 import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
 import ForYouFeed from "./ForYouFeed";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FollowingFeed from "./FollowingFeed";
 
 export default function Home() {
 
@@ -11,7 +12,18 @@ export default function Home() {
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        <ForYouFeed />
+        <Tabs defaultValue="for-you">
+          <TabsList>
+            <TabsTrigger value="for-you">For you</TabsTrigger>
+            <TabsTrigger value="following">Following</TabsTrigger>
+          </TabsList>
+          <TabsContent value="for-you">
+            <ForYouFeed />
+          </TabsContent>
+          <TabsContent value="following">
+            <FollowingFeed />
+          </TabsContent>
+        </Tabs>
       </div>
       <TrendsSidebar />
     </main>
